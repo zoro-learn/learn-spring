c8cfaddf4493f7fa27ff13a51ff821d6ad632fa3
zoro-learn <zoro.roronoa.onepiece.03@gmail.com>
2024-03-19 19:51:26 +0530

    dataBootstraping

---
+++ user-module/src/main/java/com/zoromart/com/zoromart/bootstrap/DataBootstrap.java
@@ -0,0 +1,14 @@
+package com.zoromart.com.zoromart.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Componentc
+public class DataBootstrap implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("checking the working of command line !!!");
+
+    }
+}
