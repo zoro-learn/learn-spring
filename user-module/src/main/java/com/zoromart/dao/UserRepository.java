fbcbf06568cb7a9f4284c5d67ddce61d41e66e39
zoro-learn <zoro.roronoa.onepiece.03@gmail.com>
2024-03-19 20:03:47 +0530

    code causing issue-2

---
+++ user-module/src/main/java/com/zoromart/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.zoromart.dao;
+
+import com.zoromart.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
--- user-module/src/main/java/com/zoromart/service/UserServiceImpl.java
+++ user-module/src/main/java/com/zoromart/service/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.zoromart.service;
 
+import com.zoromart.dao.UserRepository;
 import com.zoromart.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public String serviceTest(User data) {
         return "working of service -- "+data.getFullName();
